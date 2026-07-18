6c2598d8c11bab0ae83d14220f3ff8384662f4fb
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-19 01:51:10 +0600

    added assets connection for the theme in functions file

---
+++ wp-content/themes/taxicab/functions.php
@@ -0,0 +1,100 @@
+<?php
+
+function mytheme_assets() {
+
+    // CSS
+
+    wp_enqueue_style(
+        'bootstrap',
+        get_template_directory_uri() . '/assets/css/bootstrap.css',
+        array(),
+        '5.0.0'
+    );
+
+    wp_enqueue_style(
+        'owl-carousel',
+        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css',
+        array(),
+        '2.3.4'
+    );
+
+    wp_enqueue_style(
+        'owl-theme',
+        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css',
+        array('owl-carousel'),
+        '2.3.4'
+    );
+
+    wp_enqueue_style(
+        'bootstrap-icons',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
+        array(),
+        '1.10.5'
+    );
+
+    wp_enqueue_style(
+        'fontawesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+        array(),
+        '6.5.1'
+    );
+
+    wp_enqueue_style(
+        'aos-css',
+        'https://unpkg.com/aos@2.3.1/dist/aos.css',
+        array(),
+        '2.3.1'
+    );
+
+    wp_enqueue_style(
+        'main-style',
+        get_template_directory_uri() . '/assets/css/style.css',
+        array('bootstrap'),
+        '1.0'
+    );
+
+
+    // JavaScript
+
+    wp_enqueue_script(
+        'jquery'
+    );
+
+    wp_enqueue_script(
+        'bootstrap-js',
+        get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js',
+        array('jquery'),
+        '5.0.0',
+        true
+    );
+
+    wp_enqueue_script(
+        'owl-carousel-js',
+        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js',
+        array('jquery'),
+        '2.3.4',
+        true
+    );
+
+    wp_enqueue_script(
+        'aos-js',
+        'https://unpkg.com/aos@2.3.1/dist/aos.js',
+        array(),
+        '2.3.1',
+        true
+    );
+
+    wp_enqueue_script(
+        'main-js',
+        get_template_directory_uri() . '/assets/js/script.js',
+        array('jquery', 'bootstrap-js', 'owl-carousel-js', 'aos-js'),
+        '1.0',
+        true
+    );
+
+}
+
+add_action(
+    'wp_enqueue_scripts',
+    'mytheme_assets'
+);
