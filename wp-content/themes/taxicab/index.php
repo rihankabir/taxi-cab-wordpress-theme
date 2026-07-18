2495c1fdc5113b1682ff5e1a5dd1f594b6fcb611
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-19 01:55:16 +0600

    removed cdn and file links because we added connections for this in functions file

--- wp-content/themes/taxicab/index.php
+++ wp-content/themes/taxicab/index.php
@@ -4,14 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>TAXI CAB</title>
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
-     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-     <link rel="stylesheet" href="css/bootstrap.css">
-     <link rel="stylesheet" href="css/style.css">
-     <link rel="stylesheet"
+    
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 	<script src="https://kit.fontawesome.com/e0e130f4dc.js" crossorigin="anonymous"></script>
 </head>
