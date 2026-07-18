ff5bd75db0ac17b7b002dd9ddd53772cd1015b5d
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-19 02:52:48 +0600

    updated the functions.php file

--- wp-content/themes/taxicab/functions.php
+++ wp-content/themes/taxicab/functions.php
@@ -18,111 +18,117 @@ function taxi_cab_setup() {
 add_action( 'after_setup_theme', 'taxi_cab_setup' );
 
 
-function mytheme_assets() {
 
-    // CSS
+function mytheme_assets(){
 
+
+    // Main CSS
     wp_enqueue_style(
-        'bootstrap',
-        get_template_directory_uri() . '/assets/css/bootstrap.css',
-        array(),
-        '5.0.0'
+        'bootstrap-css',
+        get_template_directory_uri() . '/assets/css/bootstrap.css'
     );
 
-    wp_enqueue_style(
-        'owl-carousel',
-        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css',
-        array(),
-        '2.3.4'
-    );
 
     wp_enqueue_style(
-        'owl-theme',
-        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css',
-        array('owl-carousel'),
-        '2.3.4'
+        'owl-carousel-css',
+        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'
     );
 
-    wp_enqueue_style(
-        'bootstrap-icons',
-        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
-        array(),
-        '1.10.5'
-    );
 
     wp_enqueue_style(
-        'fontawesome',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
-        array(),
-        '6.5.1'
+        'owl-theme-css',
+        'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css'
     );
+
 
     wp_enqueue_style(
         'aos-css',
-        'https://unpkg.com/aos@2.3.1/dist/aos.css',
-        array(),
-        '2.3.1'
+        'https://unpkg.com/aos@2.3.1/dist/aos.css'
     );
 
+
     wp_enqueue_style(
-        'main-style',
-        get_template_directory_uri() . '/assets/css/style.css',
-        array('bootstrap'),
-        '1.0'
+        'bootstrap-icons',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'
     );
+
+
+    wp_enqueue_style(
+        'fontawesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+    );
+
  wp_enqueue_style(
         'theme-style',
         get_stylesheet_uri(),
         array(),
-        '1.0.0'
+        wp_get_theme()->get( 'Version' )
+    );
+    
+    wp_enqueue_style(
+        'main-style',
+        get_template_directory_uri() . '/assets/css/style.css'
     );
 
 
-    // JavaScript
 
-    wp_enqueue_script(
-        'jquery'
-    );
 
+
+    // WordPress already has jQuery
+    wp_enqueue_script('jquery');
+
+
+
+    // Bootstrap JS
     wp_enqueue_script(
         'bootstrap-js',
         get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js',
         array('jquery'),
-        '5.0.0',
+        null,
         true
     );
 
+
+
+    // Owl Carousel JS
     wp_enqueue_script(
         'owl-carousel-js',
         'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js',
         array('jquery'),
-        '2.3.4',
+        null,
         true
     );
 
+
+
+    // AOS JS
     wp_enqueue_script(
         'aos-js',
         'https://unpkg.com/aos@2.3.1/dist/aos.js',
         array(),
-        '2.3.1',
+        null,
         true
     );
 
+
+
+    // Your custom JS
     wp_enqueue_script(
         'main-js',
-        get_template_directory_uri() . '/assets/js/script.js',
-        array('jquery', 'bootstrap-js', 'owl-carousel-js', 'aos-js'),
-        '1.0',
+        get_template_directory_uri() . '/assets/js/main.js',
+        array('jquery'),
+        null,
         true
     );
 
+
 }
+
 
 add_action(
     'wp_enqueue_scripts',
     'mytheme_assets'
 );
-
-
+   
 
 ?>
