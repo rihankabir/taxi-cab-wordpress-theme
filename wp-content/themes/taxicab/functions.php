47245fa23bba1c725010f2fabc9637a04692962f
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-19 21:48:57 +0600

    added owl carousel enqueue script in functions file

--- wp-content/themes/taxicab/functions.php
+++ wp-content/themes/taxicab/functions.php
@@ -22,13 +22,41 @@ add_action( 'after_setup_theme', 'taxi_cab_setup' );
 
 function taxi_assets(){
  wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(),'5.0.3', 'all' );
+ wp_enqueue_style( 'aos_css', get_template_directory_uri() . '/assets/aos/aos.css', array(),'', 'all' );
+ wp_enqueue_style(
+    'fontawesome',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+    array(),
+    '6.5.1'
+);
+wp_enqueue_style(
+    'owl-carousel',
+    get_template_directory_uri() . '/assets/css/owl.carousel.min.css',
+    array(),
+    '2.3.4'
+);
+
+wp_enqueue_style(
+    'owl-theme',
+    get_template_directory_uri() . '/assets/css/owl.theme.default.min.css',
+    array('owl-carousel'),
+    '2.3.4'
+);
 wp_enqueue_style( 'template_css', get_template_directory_uri() . '/assets/css/style.css', array(),'', 'all' );
 wp_enqueue_style( 'theme_css', get_template_directory_uri() );
 
 
- wp_enqueue_script( 'jquery_js', get_template_directory_uri() . '/assets/js/jquery-3.7.1.min.js', array(), '3.7.1', true );
-wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.js', array(), '1.0.0', true );
 
+ wp_enqueue_script( 'jquery_js', get_template_directory_uri() . '/assets/js/jquery-3.7.1.min.js', array(), '3.7.1', true );
+ wp_enqueue_script(
+    'owl-carousel',
+    get_template_directory_uri() . '/assets/js/owl.carousel.min.js',
+    array('jquery'),
+    '2.3.4',
+    true
+);
+wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/assets/js/js/bootstrap.bundle.min.js', array(), '1.0.0', true );
+wp_enqueue_script( 'aos_js', get_template_directory_uri() . '/assets/aos/aos.js', array(), '1.0.0', true );
    wp_enqueue_script( 'script_js', get_template_directory_uri() . '/assets/js/script.js', array(), '', true );
 }
 add_action('wp_enqueue_scripts','taxi_assets');
