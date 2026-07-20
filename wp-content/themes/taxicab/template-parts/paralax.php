9e6c24b7e6398d34de88c01e4b1f5b1706b11908
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-20 19:27:27 +0600

    moved the paralax section from the index file to newly created paralax template file

---
+++ wp-content/themes/taxicab/template-parts/paralax.php
@@ -0,0 +1,87 @@
+<section class="paralax">
+<div class="container">
+  <h2 class="text-center ptext">Get More Benefits</h2>
+<h1 class="text-center text-bold p1txt">DOWNLOAD THE APP</h1>
+  <div class="row">
+<div class="col-md-4">
+
+    <!-- Item 1 -->
+    <div class="promo-box mt-5 d-flex align-items-start mb-4" data-aos="fade-right">
+
+        <div class="promo">
+            <span class="num">01</span>
+        </div>
+
+        <div class="promotxt">
+            <h2 class="txtt1">FAST BOOKING</h2>
+
+            <h3 class="white">
+                Nam ac ligula congue, interdum enim sit amet, fermentum nisi.
+            </h3>
+        </div>
+
+    </div>
+
+    <!-- Item 2 -->
+    <div class="promo-box d-flex align-items-start" data-aos="fade-right">
+
+        <div class="promo">
+            <span class="num">02</span>
+        </div>
+
+        <div class="promotxt">
+            <h2 class="txtt1">EASY TO USE</h2>
+
+            <h3 class="white">
+                Nam ac ligula congue, interdum enim sit amet, fermentum nisi.
+            </h3>
+        </div>
+
+    </div>
+
+</div>
+<div class="col-md-4">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/taxiapp.png" alt="taxi cab mobile app" class="img-fluid mt-3 w-100" data-aos="fade-up">
+</div>
+<div class="col-md-4">
+
+    <!-- Item 1 -->
+    <div class="promo-box mt-5 d-flex align-items-start mb-4" data-aos="fade-left">
+
+        <div class="promo">
+            <span class="num">03</span>
+        </div>
+
+        <div class="promotxt">
+            <h2 class="txtt1">GPS SEARCHING</h2>
+
+            <h3 class="white">
+                Nam ac ligula congue, interdum enim sit amet, fermentum nisi.
+            </h3>
+        </div>
+
+    </div>
+
+    <!-- Item 2 -->
+    <div class="promo-box d-flex align-items-start" data-aos="fade-left">
+
+        <div class="promo">
+            <span class="num">04</span>
+        </div>
+
+        <div class="promotxt">
+            <h2 class="txtt1">BONUSES FOR RIDE</h2>
+
+            <h3 class="white">
+                Nam ac ligula congue, interdum enim sit amet, fermentum nisi.
+            </h3>
+        </div>
+
+    </div>
+
+</div>
+
+
+  </div>
+</div>
+</section>
