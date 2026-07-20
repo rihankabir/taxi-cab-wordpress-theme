a9be7a0953091ab8a8fdd13fd18f75b2b8d1fae3
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-20 20:23:22 +0600

    created a brand template part file for brand section

--- wp-content/themes/taxicab/front-page.php
+++ wp-content/themes/taxicab/front-page.php
@@ -40,69 +40,7 @@ get_header();
 <!--promod section ends-->
 
 <!--brand partner section starts-->
-<section class="brands py-5">
 
-    <div class="container">
-
-        <div class="row align-items-center g-4">
-
-            <!-- Left Content -->
-            <div class="col-12 col-lg-3 text-center text-lg-start">
-
-                <h5 class="brandtxt">
-                    OUR PARTNERS
-                </h5>
-
-                <h2 class="brandtxt1">
-                    AND CLIENTS
-                </h2>
-
-            </div>
-
-            <!-- Brand Logos -->
-            <div class="col-12 col-lg-9">
-
-                <div class="row g-4 justify-content-center">
-
-                    <div class="col-6 col-sm-4 col-md-4 col-lg">
-                        <div class="brand-item">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand1.jpg" class="img-fluid" alt="Brand 1">
-                        </div>
-                    </div>
-
-                    <div class="col-6 col-sm-4 col-md-4 col-lg">
-                        <div class="brand-item">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand2.jpg" class="img-fluid" alt="Brand 2">
-                        </div>
-                    </div>
-
-                    <div class="col-6 col-sm-4 col-md-4 col-lg">
-                        <div class="brand-item">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand3.jpg" class="img-fluid" alt="Brand 3">
-                        </div>
-                    </div>
-
-                    <div class="col-6 col-sm-4 col-md-4 col-lg">
-                        <div class="brand-item">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand4.jpg" class="img-fluid" alt="Brand 4">
-                        </div>
-                    </div>
-
-                    <div class="col-6 col-sm-4 col-md-4 col-lg">
-                        <div class="brand-item">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand5.jpg" class="img-fluid" alt="Brand 5">
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
 <!--brand partner section ends-->
 <!--footer section starts-->
 <?php get_footer(); ?>
