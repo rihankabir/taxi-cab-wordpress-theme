68a328cb0b0999e8aedea74e3fbd04e78b03b876
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-21 03:54:24 +0600

    called the template parts on index file so that all sections arrive

--- wp-content/themes/taxicab/front-page.php
+++ wp-content/themes/taxicab/front-page.php
@@ -8,39 +8,44 @@ get_header();
      HERO SLIDER START
 ========================= -->
 
+<?php get_template_part( 'template-parts/hero' );?>
     <!-- NEXT BUTTON -->
    
 <!--about us section start-->
-
+<?php get_template_part( 'template-parts/about' );?>
 
 <!--about us section ends-->
 
 <!--offer section starts-->
+<?php get_template_part( 'template-parts/offer' );?>
 
 <!--offer section ends-->
 
 <!--tarrif's section start-->
-
+<?php get_template_part( 'template-parts/tarrif' );?>
 
 <!--tarrif's section ends-->
 
 <!--paralax section start-->
-
+<?php get_template_part( 'template-parts/paralax' );?>
 <!--paralax section ends-->
 <!--drivers section starts-->
-
+<?php get_template_part( 'template-parts/driver' );?>
 
 <!--drivers section ends-->
 <!--testimonial section starts-->
-
+<?php get_template_part( 'template-parts/testimonial' );?>
 
 <!--testimonial section ends-->
 <!--promod section starts-->
+<?php get_template_part( 'template-parts/promod' );?>
 
 <!--promod section ends-->
-
 <!--brand partner section starts-->
-
+<?php get_template_part( 'template-parts/brand' );?>
 <!--brand partner section ends-->
 <!--footer section starts-->
+
+
+
 <?php get_footer(); ?>
