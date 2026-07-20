d209bd6073d88766b69d79493e9931a1caac6f46
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-20 19:17:26 +0600

    moved about section from index to about template file

---
+++ wp-content/themes/taxicab/template-parts/about.php
@@ -0,0 +1,22 @@
+<section class="about justify-content-center" data-aos="fade-up">
+<div class="container">
+    <div class="row">
+<div class="col-lg-6 col-md-6">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/taxi about.jpg" class="img-fluid rounded w-100 about-img" alt="about us image">
+</div>
+<div class="col-lg-6 col-md-6">
+    <h2 class="text-start about-txt">ABOUT US</h2>
+    <h1 class="text-start about-txt1 mt-3">We Provide Trusted <span class="txt3">Cab</span> <span class="txt4">Service</span> In The World</h1>
+    <p class="text-start txt5 mt-3">Welcome to “Taxi-Cab”. We are a fully licensed and well-experienced Taxi Service Provider in England.
+
+No matter if you are an individual or a group of people, large or small, business or family, we are here to facilitate you with efficient, clean, and professional Taxi Services in Fort William (England). We are available 24 hours throughout the year. All of our drivers are highly trained and certified to maintain the premier standards for the passengers. We believe that you will be back again to get the services from us in Fort William (England). 
+</p>
+
+<a href="about.html" class="btn about-btn">Discover More</a>
+</div>
+
+
+
+    </div>
+</div>
+</section>
