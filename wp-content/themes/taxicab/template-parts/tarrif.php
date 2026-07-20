6ac1f00c5abf0998cc6c7ccdbc107f5e0009d2c2
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-20 19:24:26 +0600

    moved tarrif section from the index file to newly created tarrif template file

---
+++ wp-content/themes/taxicab/template-parts/tarrif.php
@@ -0,0 +1,85 @@
+<section class="tarrif" data-aos="fade-up">
+    <div class="container">
+        <h1 class="text-center"><span class="TRTEXT">SEE</span> OUR <span class="TRTEXT1">TARIFFS</span></h1>
+        <div class="row g-4 mt-5">
+            <!-- Card 1 -->
+      <div class="col-lg-3 col-md-6">
+        <div class="tariff-card">
+
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/taxi11.jpg" alt="taxi">
+
+          <h3>STANDARD</h3>
+
+          <p>
+            Standard sedan for a drive around the city at your service
+          </p>
+
+          <div class="price">
+            $2<span>/km</span>
+          </div>
+
+        </div>
+      </div>
+  <!-- Card 2 -->
+      <div class="col-lg-3 col-md-6">
+        <div class="tariff-card">
+
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/taxi12.jpg" alt="taxi">
+
+          <h3>BUSINESS</h3>
+
+          <p>
+            Standard sedan for a drive around the city at your service
+          </p>
+
+          <div class="price">
+            $2.7<span>/km</span>
+          </div>
+
+        </div>
+      </div>
+
+      <!-- Card 3 Active -->
+      <div class="col-lg-3 col-md-6">
+        <div class="tariff-card active-card">
+
+          <div class="corner">
+            <i class="fa-solid fa-star"></i>
+          </div>
+
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/taxi13.jpg" alt="vip">
+
+          <h3>VIP</h3>
+
+          <p>
+            Standard sedan for a drive around the city at your service
+          </p>
+
+          <div class="price">
+            $5<span>/km</span>
+          </div>
+
+        </div>
+      </div>
+
+      <!-- Card 4 -->
+      <div class="col-lg-3 col-md-6">
+        <div class="tariff-card">
+
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/taxi14.jpg" alt="minivan">
+
+          <h3>BUS-MINIVAN</h3>
+
+          <p>
+            Standard sedan for a drive around the city at your service
+          </p>
+
+          <div class="price">
+            $4.5<span>/km</span>
+          </div>
+
+        </div>
+      </div>
+        </div>
+    </div>
+</section>
