f36035098c0d9e2931f9e5d3c162868c0fa57d24
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-20 20:07:14 +0600

    moved the promo section from index file to newly created promo template part file

--- wp-content/themes/taxicab/front-page.php
+++ wp-content/themes/taxicab/front-page.php
@@ -35,37 +35,10 @@ get_header();
 
 
 <!--testimonial section ends-->
+<!--promod section starts-->
 
-<section class="promod">
-  <div class="container">
-    <div class="row">
-<div class="col-lg-6 col-md-6 col-12">
-  <div class="promo-banner">
+<!--promod section ends-->
 
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/promo1.jpg" alt="promo banner" class="img-fluid rounded">
-
-    <div class="hero-content">
-        <h1 class="text promotxts"><span class="worldtxt">-50%</span> ON FIRST ORDER</h1>
-        <h2 class="text">SPECIAL OFFER</h2>
-    </div>
-
-</div>
-</div>
-<div class="col-lg-6 col-md-6 col-12">
-  <div class="promo-banner">
-
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/promo2.jpg" alt="promo banner" class="img-fluid rounded">
-
-    <div class="hero-content">
-        <h1 class="text-center cartxt">Business Car Rental</h1>
-        <p class="text-center">If U need a Luxury Car for Business,We are here to provide</p>
-    </div>
-
-</div>
-</div>
-    </div>
-  </div>
-</section>
 <!--brand partner section starts-->
 <section class="brands py-5">
 
