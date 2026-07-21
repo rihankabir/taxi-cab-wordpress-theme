affe8fdd820e166884ab9b5e522b2e5459390e5f
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-21 16:41:53 +0600

    completed a meta box function for hero slider inner parts

---
+++ wp-content/themes/taxicab/inc/meta-boxes.php
@@ -0,0 +1,189 @@
+<?php
+
+function taxi_cab_add_hero_meta_boxes() {
+
+    add_meta_box(
+        'hero_slide_details',
+        'Hero Slide Details',
+        'taxi_cab_hero_meta_callback',
+        'hero_slide',
+        'normal',
+        'high'
+    );
+
+}
+
+add_action(
+    'add_meta_boxes',
+    'taxi_cab_add_hero_meta_boxes'
+);
+
+
+
+/** *
+*/
+
+function taxi_cab_hero_meta_callback( $post ) {
+
+    wp_nonce_field(
+        'hero_slide_nonce_action',
+        'hero_slide_nonce'
+    );
+
+    $small_heading = get_post_meta(
+        $post->ID,
+        '_small_heading',
+        true
+    );
+
+    $button_text = get_post_meta(
+        $post->ID,
+        '_button_text',
+        true
+    );
+
+    $button_url = get_post_meta(
+        $post->ID,
+        '_button_url',
+        true
+    );
+?>
+
+<p>
+
+<label>
+
+<strong>Small Heading</strong>
+
+</label>
+
+<br>
+
+<input
+type="text"
+name="small_heading"
+value="<?php echo esc_attr( $small_heading ); ?>"
+style="width:100%;">
+
+</p>
+
+<p>
+
+<label>
+
+<strong>Button Text</strong>
+
+</label>
+
+<br>
+
+<input
+type="text"
+name="button_text"
+value="<?php echo esc_attr( $button_text ); ?>"
+style="width:100%;">
+
+</p>
+
+<p>
+
+<label>
+
+<strong>Button URL</strong>
+
+</label>
+
+<br>
+
+<input
+type="url"
+name="button_url"
+value="<?php echo esc_url( $button_url ); ?>"
+style="width:100%;">
+
+</p>
+
+<?php
+
+}
+
+function taxi_cab_save_hero_meta( $post_id ) {
+
+    if (
+        ! isset( $_POST['hero_slide_nonce'] ) ||
+        ! wp_verify_nonce(
+            $_POST['hero_slide_nonce'],
+            'hero_slide_nonce_action'
+        )
+    ) {
+
+        return;
+
+    }
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+
+        return;
+
+    }
+
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+
+        return;
+
+    }
+
+    if ( isset( $_POST['small_heading'] ) ) {
+
+        update_post_meta(
+
+            $post_id,
+
+            '_small_heading',
+
+            sanitize_text_field(
+                $_POST['small_heading']
+            )
+
+        );
+
+    }
+
+    if ( isset( $_POST['button_text'] ) ) {
+
+        update_post_meta(
+
+            $post_id,
+
+            '_button_text',
+
+            sanitize_text_field(
+                $_POST['button_text']
+            )
+
+        );
+
+    }
+
+    if ( isset( $_POST['button_url'] ) ) {
+
+        update_post_meta(
+
+            $post_id,
+
+            '_button_url',
+
+            esc_url_raw(
+                $_POST['button_url']
+            )
+
+        );
+
+    }
+
+}
+
+add_action(
+    'save_post',
+    'taxi_cab_save_hero_meta'
+);
