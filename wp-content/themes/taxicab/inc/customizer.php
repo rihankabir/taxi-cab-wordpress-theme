f2e6d3d5c190e587917bdbe53099056c265e343d
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-26 22:26:19 +0600

    added theme customization setting for top header linkedin url customization

--- wp-content/themes/taxicab/inc/customizer.php
+++ wp-content/themes/taxicab/inc/customizer.php
@@ -648,6 +648,12 @@ $wp_customize->add_control(
         'type'    => 'url',
     )
 );
+$wp_customize->add_setting(
+    'linkedin_url',
+    array(
+        'sanitize_callback' => 'esc_url_raw',
+    )
+);
 }
 
 add_action(
