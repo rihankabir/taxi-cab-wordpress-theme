c6dabd202c4cd2dcdfa68bbac17eed165b6149b1
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-26 22:21:46 +0600

    added customizer setting for the top header section twitter url

--- wp-content/themes/taxicab/inc/customizer.php
+++ wp-content/themes/taxicab/inc/customizer.php
@@ -620,6 +620,12 @@ $wp_customize->add_control(
         'type'    => 'url',
     )
 );
+$wp_customize->add_setting(
+    'twitter_url',
+    array(
+        'sanitize_callback' => 'esc_url_raw',
+    )
+);
 }
 
 add_action(
