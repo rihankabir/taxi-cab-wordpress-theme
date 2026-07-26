cdd2600ba2fba06b4d43831e5fb0bed7c25d1dc9
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-26 22:20:43 +0600

    added control for the top header facebook url customization

--- wp-content/themes/taxicab/inc/customizer.php
+++ wp-content/themes/taxicab/inc/customizer.php
@@ -612,6 +612,14 @@ $wp_customize->add_setting(
         'sanitize_callback' => 'esc_url_raw',
     )
 );
+$wp_customize->add_control(
+    'facebook_url',
+    array(
+        'label'   => 'Facebook URL',
+        'section' => 'top_header_section',
+        'type'    => 'url',
+    )
+);
 }
 
 add_action(
