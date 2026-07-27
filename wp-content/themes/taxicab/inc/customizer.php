c9ec2d5105ece5a3e3d74377db6581360929421e
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-27 19:06:08 +0600

    added customizer setting and control for about page about banner main heading

--- wp-content/themes/taxicab/inc/customizer.php
+++ wp-content/themes/taxicab/inc/customizer.php
@@ -872,6 +872,35 @@ $wp_customize->add_control(
     )
 
 );
+$wp_customize->add_setting(
+
+    'about_banner_heading',
+
+    array(
+
+        'default' => 'About Us',
+
+        'sanitize_callback' => 'sanitize_text_field',
+
+    )
+
+);
+
+$wp_customize->add_control(
+
+    'about_banner_heading',
+
+    array(
+
+        'label' => 'Main Heading',
+
+        'section' => 'about_page_banner',
+
+        'type' => 'text',
+
+    )
+
+);
 }
 
 add_action(
