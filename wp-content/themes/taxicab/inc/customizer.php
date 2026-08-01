731a28267f5ab68e3638c342e958adaa3287ff5f
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-08-01 18:27:38 +0600

    added theme customizer setting for the contact page home text

--- wp-content/themes/taxicab/inc/customizer.php
+++ wp-content/themes/taxicab/inc/customizer.php
@@ -1333,6 +1333,19 @@ $wp_customize->add_control(
     )
 
 );
+$wp_customize->add_setting(
+
+    'contact_breadcrumb_home',
+
+    array(
+
+        'default'           => 'Home',
+
+        'sanitize_callback' => 'sanitize_text_field',
+
+    )
+
+);
 
 
 }
